0d848abc468bdeafa798876189dbe99b3dbaea97
Ubuntu <ubuntu@ip-172-31-31-171.ec2.internal>
2019-07-25 15:50:49 +0000

    Update BERT notebook

--- 07_bert_app/utils.py
+++ 07_bert_app/utils.py
@@ -121,3 +121,83 @@ class EncoderBlock(gluon.nn.Block):
     def forward(self, X, mask):
         Y = self.add_1(X, self.attention(X, X, X, mask))
         return self.add_2(Y, self.ffn(Y))
+
+def train_loop(net, train_data, test_data, num_epoch, lr, ctx, loss_fn):
+    trainer = gluon.Trainer(net.collect_params(), 'bertadam', {'learning_rate': lr, 'wd':0.01})
+    params = [p for p in net.collect_params().values() if p.grad_req != 'null']
+    grad_clip = 1
+    
+    num_warmup_steps = 50
+    step_num = 0
+    num_train_steps = len(train_data) * num_epoch
+    
+    for epoch in range(num_epoch):
+        accuracy = mx.metric.Accuracy()
+        running_loss = 0
+        for i, (inputs, seq_len, token_types, label) in enumerate(train_data):
+            step_num += 1
+    
+            # learning rate schedule
+            if step_num < num_warmup_steps:
+                new_lr = lr * step_num / num_warmup_steps
+            else:
+                non_warmup_steps = step_num - num_warmup_steps
+                offset = non_warmup_steps / (num_train_steps - num_warmup_steps)
+                new_lr = lr - offset * lr
+            trainer.set_learning_rate(new_lr)
+            
+            inputs = inputs.as_in_context(ctx)
+            seq_len = seq_len.as_in_context(ctx)
+            token_types = token_types.as_in_context(ctx)
+            label = label.as_in_context(ctx)
+            with mx.autograd.record():
+                out = net(inputs, token_types, seq_len)
+                loss = loss_fn(out, label.astype('float32'))
+            loss.backward()
+            running_loss += loss.mean()
+            # Gradient clipping
+            trainer.allreduce_grads()
+            nlp.utils.clip_grad_global_norm(params, 1)
+            trainer.update(1)
+    
+            accuracy.update(label, out.softmax())
+            
+            if i % 50 == 0:
+                print("Batch", i, "Accuracy", accuracy.get()[1],"Loss", running_loss.asscalar()/(i+1))
+        print("Epoch {}, Accuracy {}, Loss {}".format(epoch, accuracy.get(), running_loss.asscalar()/(i+1)))
+        evaluate(test_data, ctx, net)
+
+def evaluate(test_data, ctx, net):
+    accuracy = 0
+    for i, (inputs, seq_len, token_types, label) in enumerate(test_data):
+        inputs = inputs.as_in_context(ctx)
+        seq_len = seq_len.as_in_context(ctx)
+        token_types = token_types.as_in_context(ctx)
+        label = label.as_in_context(ctx)
+        out = net(inputs, token_types, seq_len)
+        accuracy += (out.argmax(axis=1).squeeze() == label).mean()
+        # if i % 50 == 0 and i > 0:
+        #     print(accuracy.asscalar()/(i+1))
+    print("Test Accuracy {}".format(accuracy.asscalar()/(i+1)))
+
+def predict_sentiment(net, ctx, vocabulary, bert_tokenizer, sentence):
+    max_len = 128
+    padding_id = vocabulary[vocabulary.padding_token]
+
+    transform = nlp.data.BERTSentenceTransform(bert_tokenizer, max_len, pad=False, pair=False)
+    dataset = gluon.data.SimpleDataset([[sentence]])
+    dataset = dataset.transform(transform)
+    batchify_fn = nlp.data.batchify.Tuple(
+        nlp.data.batchify.Pad(axis=0, pad_val=padding_id),
+        nlp.data.batchify.Stack(),
+        nlp.data.batchify.Pad(axis=0, pad_val=padding_id))
+    predict_data = gluon.data.DataLoader(dataset, batchify_fn=batchify_fn,
+                                         batch_size=1)
+
+    for i, (inputs, seq_len, token_types) in enumerate(predict_data):
+        inputs = mx.nd.array(inputs).as_in_context(ctx)
+        token_types = mx.nd.array(token_types).as_in_context(ctx)
+        seq_len = mx.nd.array(seq_len, dtype='float32').as_in_context(ctx)
+        out = net(inputs, token_types, seq_len)
+        label = nd.argmax(out, axis=1)
+        return 'positive' if label.asscalar() == 1 else 'negative'
